08b879aecc09651bbd346ee15903a8868802e180
Michal Wallace <michal.wallace@gmail.com>
2010-01-17 09:14:08 +0000

    added Assets.hx
    git-svn-id: file:///svn/svn/alchementrix/trunk/game1@617 fd047da9-ae66-0410-9357-e790289b581a

---
+++ code/Assets.hx
@@ -0,0 +1,41 @@
+﻿import flash.display.MovieClip;
+
+class Assets {}
+class ClockFont extends flash.text.Font {}
+
+class BorderClip extends MovieClip {}
+class GlassLayer extends MovieClip {}
+
+class BG0000 extends MovieClip {}
+class BG0001 extends MovieClip {}
+class BG0002 extends MovieClip {}
+class BG0003 extends MovieClip {}
+class BG0004 extends MovieClip {}
+class BG0005 extends MovieClip {}
+class BG0006 extends MovieClip {}
+class BG0007 extends MovieClip {}
+class BG0009 extends MovieClip {}
+
+class FG0000 extends MovieClip {}
+class FG0001 extends MovieClip {}
+class FG0002 extends MovieClip {}
+class FG0003 extends MovieClip {}
+class FG0004 extends MovieClip {}
+class FG0005 extends MovieClip {}
+class FG0006 extends MovieClip {}
+class FG0007 extends MovieClip {}
+class FG0009 extends MovieClip {}
+
+class ShepClip extends MovieClip {}
+class GlowClip extends MovieClip {}
+class BallClip extends MovieClip {}
+class PocketClip extends MovieClip {}
+class DoorClip extends MovieClip {}
+class SpinnerClip extends MovieClip {}
+class RedBallClip extends MovieClip {}
+class RedPocketClip extends MovieClip {}
+class CargoClip extends MovieClip {}
+class CrateClip extends MovieClip {}
+
+class SoundIcon extends MovieClip {}
+class MuteIcon extends MovieClip {}
